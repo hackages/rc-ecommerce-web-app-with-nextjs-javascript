ec216e0f98c4c5609db7dce1374c3bfbc327834c
Davy Engone <davy@hackages.io>
2022-08-19 15:38:26 +0200

    removed dead code and fixed imports in Admin page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import ProductForm from '../components/Product/ProductForm';
+import {ProductForm} from '../components';
 
 const AdminPage = () => <ProductForm />
 
